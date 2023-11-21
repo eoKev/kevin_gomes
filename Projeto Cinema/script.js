edf84b9c47013d8ffbbab3f1c234b9b472d7a989
Kevin <139785290+eoKev@users.noreply.github.com>
2023-11-21 00:02:49 -0300

    Update script.js
    Funções para gerenciar a escolha de assentos e simular a compra de ingresso, também para gerenciar as janelas de pop up

--- Projeto Cinema/script.js
+++ Projeto Cinema/script.js
@@ -1,20 +1,68 @@
-const container = document.querySelectorAll(".container");
-
-container.addEventListener("click", (e) => {
-    if (e.target.classList.contains("assento") && !e.target.classList.contains("ocupado")) {
-        e.target.classList.toggle("selecionado");
-
-    }
-});
+const container = document.querySelector(".container");
+const assentosSelecionados = document.getElementById("qtdAssentos");
+const totalPago = document.getElementById("total");
 
 document.addEventListener("DOMContentLoaded", () => {
-    const rows = document.querySelectorAll("tr[data-href");
-    rows.forEach(row => {
-        row.addEventListener("click", () =>{
-            window.location.href = row.dataset.href;
-        });
-    });
+  container.addEventListener("click", (e) => {
+    const assento = e.target.closest(".assento:not(.ocupado)");
+
+    if (assento) {
+      assento.classList.toggle("selecionado");
+      updateSelectedCount();
+    }
+  });
 });
 
+function updateSelectedCount() {
+  const assentos = document.querySelectorAll(".assento.selecionado:not(.info .assento.selecionado)");
+  const quantidadeAssentos = assentos.length;
+  const precoUnitario = parseFloat(document.getElementById("opcao").value);
+  const precoTotal = quantidadeAssentos * precoUnitario;
+
+  assentosSelecionados.textContent = quantidadeAssentos;
+  totalPago.textContent = precoTotal.toFixed(2);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rows = document.querySelectorAll("tr[data-href]");
+  rows.forEach((row) => {
+    row.addEventListener("click", () => {
+      window.location.href = row.dataset.href;
+    });
+  });
+});
+
+function exibirResumoCompra() {
+  const opcaoSelecionadaElement = document.getElementById("opcao");
+  const opcaoSelecionada = opcaoSelecionadaElement.options[opcaoSelecionadaElement.selectedIndex].text;
+  const qtdAssentos = document.getElementById("qtdAssentos").textContent;
+  const total = document.getElementById("total").textContent;
+
+  document.getElementById("modalOpcao").textContent = opcaoSelecionada;
+  document.getElementById("modalQtdAssentos").textContent = qtdAssentos;
+  document.getElementById("modalTotal").textContent = total;
+
+  document.getElementById("modal").style.display = "block";
+}
+
+function fecharModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+function exibirPopupSucesso() {
+  alert("Compra processada com sucesso! Aproveite o filme.");
+}
+
+function finalizarCompra() {
+  const qtdAssentos = document.getElementById("qtdAssentos").textContent;
+
+  if (qtdAssentos === "0") {
+    alert("Erro! Você deve escolher um assento.");
+  } else {
+    setTimeout(() => {
+      exibirPopupSucesso();
+    }, 1500);
+  }
+}
 
 
